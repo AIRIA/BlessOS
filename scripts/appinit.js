4412593d457465b21e22ab5b39a2dc4b91e738e5
airia <chaibingcheng0305@163.com>
2012-11-02 17:33:18 +0800

    update main.swf

--- scripts/appinit.js
+++ scripts/appinit.js
@@ -1,6 +1,7 @@
 var flashvars = {};
 var swfVersionStr = "10.2.0";
 var xiSwfUrlStr = "os/playerProductInstall.swf";
+var randomVersion = Math.random();
 var params = {
 	qulity:'high',
 	bgcolor:'#FFFFFF',
@@ -12,4 +13,4 @@ var attributes = {
 	name:'BlessOS',
 	align:'middle'
 };
-swfobject.embedSWF("os/main.swf","blessos","100%","100%",swfVersionStr,xiSwfUrlStr,flashvars,params,attributes);
+swfobject.embedSWF("os/main.swf?random="+randomVersion,"blessos","100%","100%",swfVersionStr,xiSwfUrlStr,flashvars,params,attributes);
