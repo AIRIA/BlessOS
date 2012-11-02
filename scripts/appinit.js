7d666ba0896e09bc13d24d7b3ea827cc8c27e31e
airia <chaibingcheng0305@163.com>
2012-11-02 13:46:02 +0800

    first import for pages

---
+++ scripts/appinit.js
@@ -0,0 +1,15 @@
+var flashvars = {};
+var swfVersionStr = "10.2.0";
+var xiSwfUrlStr = "os/playerProductInstall.swf";
+var params = {
+	qulity:'high',
+	bgcolor:'#FFFFFF',
+	allowscriptaccess:'sameDomain',
+	allowfullscreen:'true'
+};
+var attributes = {
+	id:'BlessOS',
+	name:'BlessOS',
+	align:'middle'
+};
+swfobject.embedSWF("os/main.swf","blessos","100%","100%",swfVersionStr,xiSwfUrlStr,flashvars,params,attributes);
